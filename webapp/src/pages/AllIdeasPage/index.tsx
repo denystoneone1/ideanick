a73f849f006fc268b756a4d1f18584b57717ebc6
alexstone-wq <agluhov50@gmail.com>
2025-08-04 06:31:47 +0500

    feat: add React Route

--- webapp/src/pages/AllIdeasPage/index.tsx
+++ webapp/src/pages/AllIdeasPage/index.tsx
@@ -1,4 +1,6 @@
+import { Link } from 'react-router-dom'
 import { trpc } from '../../lib/trpc'
+import { getViewIdeaRoute } from '../../lib/routes'
 
 export const AllIdeasPage = () => {
   const { data, error, isLoading, isFetching, isError } = trpc.getIdeas.useQuery()
@@ -10,7 +12,9 @@ export const AllIdeasPage = () => {
       <h1>All Ideas</h1>
       {data.ideas.map((idea) => (
         <div key={idea.nick}>
-          <h2>{idea.name}</h2>
+          <h2>
+            <Link to={getViewIdeaRoute({ ideaNick: idea.nick })}>{idea.name}</Link>
+          </h2>
           <p>{idea.description}</p>
         </div>
       ))}
